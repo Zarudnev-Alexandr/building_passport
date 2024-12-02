9fe6af01675bf8c571816c90575b3953c481f6a1
Zarudnev-Alexandr <alexandrzarudnev57@gmail.com>
2024-12-02 18:36:49 +0300

    начало

---
+++ app/app/settings.py
@@ -0,0 +1,247 @@
+import os
+from datetime import timedelta
+from pathlib import Path
+
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = env("SECRET_KEY")
+
+DEBUG = bool(int(env("DEBUG")))
+
+ALLOWED_HOSTS = [
+    "*",
+]
+
+if DEBUG:
+    CSRF_TRUSTED_ORIGINS = [
+        # Для самого Django
+        "http://localhost:8000",
+        "http://127.0.0.1:8000",
+        # Для React
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "http://localhost:5860",
+    ]
+# else:
+#     CSRF_TRUSTED_ORIGINS = [
+#         # Prod
+#         "https://gristdwh.orel-ecenter.ru",
+#         "https://zkh.orel-ecenter.ru",
+#         "https://erazkh.orel-ecenter.ru",
+#         # Test
+#         "http://localhost:5860",
+#     ]
+
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'corsheaders',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
+    'storages',
+    'buildings',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+
+ROOT_URLCONF = 'app.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'app.wsgi.application'
+ASGI_APPLICATION = "app.asgi.application"
+
+match DEBUG:
+    case True:
+        DATABASES = {
+            "default": {
+                "ENGINE": "django.db.backends.sqlite3",
+                "NAME": BASE_DIR / "db.sqlite3",
+            }
+        }
+    # case False:
+    #     DATABASES = {
+    #         "default": {
+    #             "ENGINE": "django.db.backends.postgresql",
+    #             "NAME": env("DB_NAME"),
+    #             "USER": env("DB_USER"),
+    #             "PASSWORD": env("DB_PASSWORD"),
+    #             "HOST": env("DB_HOST"),
+    #             "PORT": env("DB_PORT"),
+    #         }
+    #     }
+
+# CHANNEL_LAYERS = {
+#     "default": {
+#         "BACKEND": "channels_redis.core.RedisChannelLayer",
+#         "CONFIG": {
+#             "hosts": [(env("REDIS_URL"), int(env("REDIS_PORT")))],
+#         },
+#     },
+# }
+
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+LANGUAGE_CODE = "ru-ru"
+TIME_ZONE = "Europe/Moscow"
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+DATE_INPUT_FORMATS = ("%d/%m/%Y", "%d-%m-%Y")
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
+
+# AWS_ACCESS_KEY_ID = env("FILE_STORAGE_ACCESS_KEY")
+# AWS_SECRET_ACCESS_KEY = env("FILE_STORAGE_SECRET_KEY")
+# AWS_S3_ENDPOINT_URL = "http://localhost:9000" if DEBUG else "http://minio:9000"
+# AWS_STORAGE_BUCKET_NAME = "static"
+# AWS_S3_CUSTOM_DOMAIN = (
+#     f"{'localhost:5860' if DEBUG else 'erazkh.orel-ecenter.ru'}/static"
+# )
+# AWS_S3_URL_PROTOCOL = "http:" if DEBUG else "https:"
+# AWS_S3_FILE_OVERWRITE = False
+#
+# FILE_UPLOAD_HANDLERS = [
+#     "django.core.files.uploadhandler.TemporaryFileUploadHandler",
+# ]
+
+
+STATIC_URL = 'static/'
+MEDIA_URL = "/media/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+    # os.path.join(BASE_DIR, "app_ui", "build", "static"),
+)
+
+IMAGES_DIR = os.path.join(MEDIA_ROOT, "images")
+if not os.path.exists(MEDIA_ROOT) or not os.path.exists(IMAGES_DIR):
+    os.makedirs(IMAGES_DIR)
+
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    ],
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+        "rest_framework.renderers.BrowsableAPIRenderer",
+        "drf_excel.renderers.XLSXRenderer",
+    ],
+}
+
+if not DEBUG:
+    REST_FRAMEWORK.update(
+        {
+            "DEFAULT_RENDERER_CLASSES": (
+                "rest_framework.renderers.JSONRenderer",
+                "drf_excel.renderers.XLSXRenderer",
+            )
+        }
+    )
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=90),
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
+    "UPDATE_LAST_LOGIN": False,
+    "ALGORITHM": "HS256",
+    "SIGNING_KEY": SECRET_KEY,
+    "VERIFYING_KEY": None,
+    "AUDIENCE": None,
+    "ISSUER": None,
+    "JWK_URL": None,
+    "LEEWAY": 0,
+    "AUTH_HEADER_TYPES": ("Bearer",),
+    "AUTH_HEADER_NAME": "HTTP_AUTHORIZATION",
+    "USER_ID_FIELD": "id",
+    "USER_ID_CLAIM": "user_id",
+    "USER_AUTHENTICATION_RULE": "rest_framework_simplejwt.authentication.default_user_authentication_rule",
+    "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
+    "TOKEN_TYPE_CLAIM": "token_type",
+    "TOKEN_USER_CLASS": "rest_framework_simplejwt.models.TokenUser",
+    "JTI_CLAIM": "jti",
+    "SLIDING_TOKEN_REFRESH_EXP_CLAIM": "refresh_exp",
+    "SLIDING_TOKEN_LIFETIME": timedelta(minutes=5),
+    "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
+}
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
